9c554d5ae74891f7f05b80ed32519822ae7f639f
Lauren Monowar-Jones <lvjones214@aol.com>
2023-03-30 13:41:41 -0400

    added short video section

---
+++ leom-suppliers/components/OurWarehouse.tsx
@@ -0,0 +1,17 @@
+const OurWarehouse=() =>{
+  return(
+    <>
+      <div className='bg-primary'>
+        <h1 className='font-bold text-4xl text-center p-12 text-white'>VIEW OUR WAREHOUSE</h1>
+        <div className='flex items-center justify-center p-4'>
+          <video controls autoPlay width='1100'>
+            <source src='LEOM_video_short.mp4' type='video/mp4'></source>
+          </video>
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default OurWarehouse;
